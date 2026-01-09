2308e400538ac2e685d584e1493523db4b331fe1
Bisma <bismakgustavo3@gmail.com>
2026-01-09 16:55:19 -0300

    fix: Web App Login and API URL

--- mobile_app/build/web/flutter_service_worker.js
+++ mobile_app/build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3fba933de95dfc2cfd7867e014f010dc",
+"flutter_bootstrap.js": "bd8513c85093a0354d9ba6347d135a25",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "21477ec9d35293a35cda1bd69f1c983d",
 "/": "21477ec9d35293a35cda1bd69f1c983d",
-"main.dart.js": "d55552f78244b7ed33a19e823d46acdd",
+"main.dart.js": "496170ae76fdaf6b8ba7db05b93b21f9",
 "manifest.json": "bffd47e6da70d36ee36eb823fa7456de",
 "version.json": "bcab1be5c6d82ad128421fe1ef985695"};
 // The application shell files that are downloaded before a service worker can
